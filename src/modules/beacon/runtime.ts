6e371cf68688b85184b69fcd00a2668c1a89a7de
gonsaje <song.jaehyun@gmail.com>
2026-04-06 17:28:14 -0400

    update BeaconRuntime

--- src/modules/beacon/runtime.ts
+++ src/modules/beacon/runtime.ts
@@ -1,9 +1,15 @@
-import { UUID } from "node:crypto";
-import { tickerApp } from "./apps/ticker";
+import { InMemoryEventBus } from "./core/bus/eventBus";
+import { App } from "./core/app/app";
+import { AppState } from "./core/app/appState";
 
+class BeaconRuntime {
+  private eventBus = new InMemoryEventBus();
+  private apps = new Map<string, { app: App; state: AppState }>();
 
-
-
-
-
-const app = tickerApp;
+  register(app: App) {
+    this.apps.set(app.id, {
+      app,
+      state: AppState.REGISTERED,
+    });
+  }
+}
